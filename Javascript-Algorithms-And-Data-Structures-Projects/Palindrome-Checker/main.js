43d2bc22e03f3ca398b72dbcf33a12b5146488b2
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-10-11 17:03:47 +0300

    Create main.js

---
+++ Javascript-Algorithms-And-Data-Structures-Projects/Palindrome-Checker/main.js
@@ -0,0 +1,22 @@
+function palindrome(str) 
+{
+  // преобразование всех букв в строке к нижнему регистру
+  let lower_case_string = str.toLowerCase();
+
+  // удаление всех символов кроме букв нижнего регистра и цифр
+  lower_case_string = lower_case_string.replace(/[^a-z0-9]/g, '');
+  
+  // переворачивание строки 
+  let reverse_lower_case_string = lower_case_string.split('').reverse().join('');
+
+  // сравнение исходной строки без пробелов и спец.символов с перевёрнутой исходной строкой без пробелов и спец.символов
+  if (lower_case_string.includes(reverse_lower_case_string))
+  {
+    return true;
+  } else
+  {
+    return false;
+  }
+}
+
+palindrome("A man, a plan, a canal. Panama.");
